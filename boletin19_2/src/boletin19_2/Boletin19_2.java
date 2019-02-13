f469c88d34d70bd18d97fe9a3ba1b3cd895510d8
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-02-13 11:27:15 +0100

    actualizado menu

--- boletin19_2/src/boletin19_2/Boletin19_2.java
+++ boletin19_2/src/boletin19_2/Boletin19_2.java
@@ -8,26 +8,30 @@ public class Boletin19_2 {
         Programacion dam = new Programacion();
         int[] notas = new int[30];
 
-        int opcion = Integer.parseInt(JOptionPane.showInputDialog("**********MENÚ**********\n"
-                + "Introduce una opción:\n"
-                + "1.- Ver número de suspensos y aprobados\n"
-                + "2.- Calcular la nota media de clase\n"
-                + "3.- Ver la nota más alta"));
+        int opcion;
 
-        switch (opcion) {
-            //opcion 1 - mostrar numero suspensos y aprobados            
-            case 1:
-                dam.clasificarNotas(notas);
-                dam.amosarAprobadosSuspensos();
-                break;
-            //opcion 2 - calcular y ver la nota media de la clase
-            case 2:
-                dam.calcularMedia(notas);
-                break;
-            //opcion 3 - ver la nota mas alta      
-            case 3:
-                dam.notaMaisAlta(notas);
-                break;
-        }
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("**********MENÚ**********\n"
+                    + "Introduce una opción:\n"
+                    + "1.- Ver número de suspensos y aprobados\n"
+                    + "2.- Calcular la nota media de clase\n"
+                    + "3.- Ver la nota más alta"));
+
+            switch (opcion) {
+                //opcion 1 - mostrar numero suspensos y aprobados            
+                case 1:
+                    dam.clasificarNotas(notas);
+                    dam.amosarAprobadosSuspensos();
+                    break;
+                //opcion 2 - calcular y ver la nota media de la clase
+                case 2:
+                    dam.calcularMedia(notas);
+                    break;
+                //opcion 3 - ver la nota mas alta      
+                case 3:
+                    dam.notaMaisAlta(notas);
+                    break;
+            }
+        } while (opcion != 0);
     }
 }
